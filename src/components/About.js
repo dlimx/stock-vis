e741b57df33956f9e7831304fe93bd00a95ad99f
David Li <david.minxiao.li@gmail.com>
2017-03-27 13:12:20 -0400

    Simple About page

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,10 @@ class About extends React.Component {
         <div className='row centered'>
           <div className='col-md-8 col-md-offset-2'>
             <h2>About</h2>
-            <p>Here is a complete list of the stocks we have available, as sourced from <a href='https://www.quandl.com'>Quandl.</a></p>
+            <p>We have daily stock data, sourced from <a href='https://www.quandl.com'>Quandl.</a></p>
+            <p>An example is shown below: </p>
+            <img src='/public/sample.jpg' />
+            <p>Technically, the server has a node.js backend, and a React front end.</p>
           </div>
         </div>
       </div>
